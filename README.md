6123407de25fd5967f023ba192461bdc6e57f758
unknown <Ryan Zeitler>
2025-06-14 00:12:55 -0400

    refactoring

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # LangChain-RAG-Chatbot
 RAG chatbot using langchain
+
+Python -m pip install -r ImportRequirements.txt
+
+
+Things to remember:
+
+        
+
+        (before work) 
+            git pull
+            Python -m pip install -r ImportRequirements.txt
+
+        (after work if like/working)
+            git add .
+            git commit -m "blah blah blah what changed"
+            git push
--- chatbotMain.py
+++ chatbotMain.py
@@ -1,6 +1,8 @@
 import os
 import getpass
 import requests
+import json
+from google.cloud import storage
 from bs4 import BeautifulSoup
 from pinecone import Pinecone, ServerlessSpec
 from dotenv import load_dotenv
@@ -12,58 +14,245 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains import create_retrieval_chain, create_history_aware_retriever
 
 load_dotenv()
-os.environ["USER_AGENT"]
 #if not os.getenv("OPENAI_API_KEY"):
   #os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
 
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-openai_api = os.getenv("OPENAI_API_KEY")
-pinecone_api = os.getenv("PINECONE_API_KEY")                    #init .env objects]
-pinecone_index_name = os.getenv("PINECONE_INDEX_NAME")
-user_agent = os.getenv("USER_AGENT")
+def env_access():
+  openai_api = os.getenv("OPENAI_API_KEY")
+  pinecone_api = os.getenv("PINECONE_API_KEY")                    #init .env objects]
+  pinecone_index_name = os.getenv("PINECONE_INDEX_NAME")
+  user_agent = os.getenv("USER_AGENT")
+  
+  return openai_api, pinecone_api, pinecone_index_name, user_agent
 
-pinecone_client = Pinecone(pinecone_api)  #init client object https://docs.pinecone.io/reference/api/authentication?utm_source=chatgpt.com
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-if pinecone_index_name not in pinecone_client.list_indexes().names():
-    pinecone_client.create_index(
-        name=pinecone_index_name,
-        dimension=1536,
-        metric="cosine",
-        spec=ServerlessSpec(cloud="aws", region="us-east-1")
-    )
+def bucket_access(bucket_name):
+  bucket_name = "aicanbucket"
+  client = storage.Client()
+  bucket = client.bucket(bucket_name)
+  
+  return bucket
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def init_pinecone(pinecone_api, pinecone_index_name):
+  pinecone_client = Pinecone(pinecone_api)  #init client object https://docs.pinecone.io/reference/api/authentication?utm_source=chatgpt.com
     
-index = pinecone_client.Index(pinecone_index_name) #use for later upsert https://docs.pinecone.io/reference/api/2024-07/data-plane/upsert?utm_source=chatgpt.com
+  if pinecone_index_name not in pinecone_client.list_indexes().names():
+    pinecone_client.create_index(
+      name=pinecone_index_name,           #checks for a pinmecone index, if not makes one
+      dimension=1536,
+      metric="cosine",
+      spec=ServerlessSpec(cloud="aws", region="us-east-1")
+  )
+    
+  index = pinecone_client.Index(pinecone_index_name) #use for later upsert https://docs.pinecone.io/reference/api/2024-07/data-plane/upsert?utm_source=chatgpt.com
+    
+  return pinecone_client, index
+  
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+def url_scrape(url):
+  response = requests.get(url)  #GET request 
+  soup = BeautifulSoup(response.content, "lxml")  #lxml recommended: faster parser and better for larger and possibly non-perfect formats https://www.crummy.com/software/BeautifulSoup/bs4/doc/#differences-between-parsers 
+  doc_links = []
 
-embeddings = OpenAIEmbeddings()  #init open ai embeddings, no arg = default embeddings from langchain and auto pull api key
+  for div in soup.find_all('div', class_ = 'region region-content'):
+      for a_tag in div.find_all("a"):
+          href = a_tag.get("href")
+          doc_links.append(href)
+          
+  return doc_links
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def doc_loader(doc_links, user_agent):
+  headers = {"User-Agent": user_agent}
+  loader= WebBaseLoader(web_paths=(doc_links),header_template=headers)
+  docs= loader.load()
+  
+  return docs
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def split_docs(docs, chunk_size=500, chunk_overlap=100):
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+  chunks = text_splitter.split_documents(docs)
+  
+  return chunks
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def main():
+  
+  openai_api, pinecone_api, pinecone_index_name, user_agent = env_access()
+  bucket = bucket_access("aicanbucket")
+  pinecone_client, pinecone_index = init_pinecone(pinecone_api, pinecone_index_name)
+  embeddings = OpenAIEmbeddings()  #init open ai embeddings, no arg = default embeddings from langchain and auto pull api key
                                  #OpenAIEmbeddings(model: str = "text-embedding-3-small",organization: Optional[str] = None,dimensions: Optional[int] = None,request_timeout: Optional[float] = None,
                                  #                 chunk_size: int = 1000,max_retries: int = 6,headers: Optional[Dict[str, str]] = None,show_progress_bar: bool = False)
+                                 
+  url = "https://www.irs.gov/irm/part2"
+  doc_links = url_scrape(url)
+  
+  docs = doc_loader(doc_links, user_agent)
+  
+  chunks = split_docs(docs)
+  
+  
+  
+  
 
 
-url = "https://www.irs.gov/irm/part2"
-response = requests.get(url)  #GET request 
-soup = BeautifulSoup(response.content, "lxml")  #lxml recommended: faster parser and better for larger and possibly non-perfect formats https://www.crummy.com/software/BeautifulSoup/bs4/doc/#differences-between-parsers 
-headers = {"User-Agent": user_agent}
+if __name__=="__main__":
+   main()
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+import os
+import getpass
+import requests
+import json
+from google.cloud import storage
+from bs4 import BeautifulSoup
+from pinecone import Pinecone, ServerlessSpec
+from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader
+#from langchain_openai import ChatOpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
 
-doc_links = []
+load_dotenv("api.env")
+#if not os.getenv("OPENAI_API_KEY"):
+  #os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
 
-for div in soup.find_all('div', class_ = 'region region-content'):
-    for a_tag in div.find_all("a"):
-        href = a_tag.get("href")
-        doc_links.append(href)
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def env_access():
+  openai_api = os.getenv("OPENAI_API_KEY")
+  pinecone_api = os.getenv("PINECONE_API_KEY")                    #init .env objects]
+  pinecone_index_name = os.getenv("PINECONE_INDEX_NAME")
+  user_agent = os.getenv("USER_AGENT")
+  
+  return openai_api, pinecone_api, pinecone_index_name, user_agent
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def bucket_access(bucket_name):
+  bucket_name = "aicanbucket"
+  client = storage.Client()
+  bucket = client.bucket(bucket_name)
+  
+  return bucket
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def init_pinecone(pinecone_api, pinecone_index_name):
+  pinecone_client = Pinecone(pinecone_api)  #init client object https://docs.pinecone.io/reference/api/authentication?utm_source=chatgpt.com
     
-loader= WebBaseLoader(web_paths=(doc_links),header_template=headers)
-docs= loader.load()
+  if pinecone_index_name not in pinecone_client.list_indexes().names():
+    pinecone_client.create_index(
+      name=pinecone_index_name,           #checks for a pinmecone index, if not makes one
+      dimension=1536,
+      metric="cosine",
+      spec=ServerlessSpec(cloud="aws", region="us-east-1")
+  )
+    
+  index = pinecone_client.Index(pinecone_index_name) #use for later upsert https://docs.pinecone.io/reference/api/2024-07/data-plane/upsert?utm_source=chatgpt.com
+    
+  return pinecone_client, index
+  
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+def url_scrape(url):
+  response = requests.get(url)  #GET request 
+  soup = BeautifulSoup(response.content, "lxml")  #lxml recommended: faster parser and better for larger and possibly non-perfect formats https://www.crummy.com/software/BeautifulSoup/bs4/doc/#differences-between-parsers 
+  doc_links = []
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-chunks = text_splitter.split_documents(docs)
+  for div in soup.find_all('div', class_ = 'region region-content'):
+      for a_tag in div.find_all("a"):
+          href = a_tag.get("href")
+          doc_links.append(href)
+          
+  return doc_links
 
-print(chunks)
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def doc_loader(doc_links, user_agent):
+  headers = {"User-Agent": user_agent}
+  loader= WebBaseLoader(web_paths=(doc_links),header_template=headers)
+  docs= loader.load()
+  
+  return docs
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def split_docs(docs, chunk_size=500, chunk_overlap=100):
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+  chunks = text_splitter.split_documents(docs)
+  
+  return chunks
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def main():
+  
+  openai_api, pinecone_api, pinecone_index_name, user_agent = env_access()
+  bucket = bucket_access("aicanbucket")
+  pinecone_client, pinecone_index = init_pinecone(pinecone_api, pinecone_index_name)
+  embeddings = OpenAIEmbeddings()  #init open ai embeddings, no arg = default embeddings from langchain and auto pull api key
+                                 #OpenAIEmbeddings(model: str = "text-embedding-3-small",organization: Optional[str] = None,dimensions: Optional[int] = None,request_timeout: Optional[float] = None,
+                                 #                 chunk_size: int = 1000,max_retries: int = 6,headers: Optional[Dict[str, str]] = None,show_progress_bar: bool = False)
+                                 
+  url = "https://www.irs.gov/irm/part2"
+  doc_links = url_scrape(url)
+  
+  docs = doc_loader(doc_links, user_agent)
+  
+  chunks = split_docs(docs)
+  
+  
+  
+  
 
 
-
-
-
-
-
-#if __name__=="__main__":
-  #  main()
+if __name__=="__main__":
+   main()
